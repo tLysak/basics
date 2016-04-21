02d9fb1859422104f301bc9cabf7b9424929634e
sgrey2001 <Xnjpfgbpltw1>
2016-04-21 22:10:56 +0300

    Initial Commit

---
+++ src/com/brainacad/sKozigon/Lesson2.java
@@ -0,0 +1,15 @@
+
+package com.brainacad.sKozigon;
+
+        import java.util.Scanner;
+
+public class Lesson2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String line1= sc.nextLine(), line2 = sc.nextLine();
+        System.out.println(line1 + line2);
+        System.out.println("Hello!");
+
+
+    }
+}

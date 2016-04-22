55318340e8cd76a1413a7e433006390333e260cb
Zemlianiy <9234455153Zima>
2016-04-22 20:43:13 +0300

    comm

---
+++ src/com/brainacad/dzemlianiy/Lesson2.java
@@ -0,0 +1,17 @@
+package com.brainacad.dzemlianiy;
+
+
+import java.util.Scanner;
+
+public class Lesson2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String a, b;
+        a = scanner.nextLine();
+        b = scanner.nextLine();
+        System.out.print(a + b);
+    }
+
+}
+
+

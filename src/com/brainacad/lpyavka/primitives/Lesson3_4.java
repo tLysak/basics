e3beb0f7618296e9810c683c1fe8eeb12d5658ec
Lena <lpyavka@gmail.com>
2016-04-24 21:04:10 +0300

    home work lesson 3

---
+++ src/com/brainacad/lpyavka/primitives/Lesson3_4.java
@@ -0,0 +1,15 @@
+package com.brainacad.lpyavka.primitives;
+
+public class Lesson3_4 {
+    public static void main(String[] args) {
+        int value = 15;
+        System.out.println("Initial value is " + value);
+
+        System.out.println("Pre increment value is " + ++value);
+        System.out.println("Post increment value is " + value++);
+        System.out.println("Final increment value is " + value);
+        System.out.println("Pre decrement value is " + --value);
+        System.out.println("Post decrement value is " + value--);
+        System.out.println("Final decrement value is " + value);
+    }
+}
---
+++ src/com/brainacad/lpyavka/primitives/Lesson3_7.java
@@ -0,0 +1,8 @@
+package com.brainacad.lpyavka.primitives;
+
+public class Lesson3_7 {
+    public static void main(String[] args) {
+        int valueTernary = 129 > -129 ? -10 : 10;
+        System.out.println(valueTernary);
+    }
+}

0f0efaebad40ad00843ad39576cd4ed423abf752
sgrey2001 <Xnjpfgbpltw1>
2016-04-25 21:45:13 +0300

    Lesson4

--- src/com/brainacad/skozigon/primitives/Lesson3_7.java
+++ src/com/brainacad/skozigon/primitives/Lesson3_7.java
@@ -7,7 +7,5 @@ public class Lesson3_7 {
     public static void main(String[] args) {
         int i = (((new Random().nextInt((int) System.currentTimeMillis())) % 2 > 0)) ? (i = 10) : (i = 20);
         System.out.println("Integer i = " + i);
-
-
     }
 }

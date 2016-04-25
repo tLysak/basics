b9076c5ef5c142a85fe799a18ae31a81bb3475f2
alexkaniv <alexkaniv"ukr.net>
2016-04-25 17:52:39 +0300

    Lesson3_5 added

---
+++ src/com/brainacad/onesterenko/primitives/Lesson3_6.java
@@ -0,0 +1,14 @@
+package com.brainacad.onesterenko.primitives;
+
+/**
+ * Оголосити і ініціалізувати 2 змінних int - 4, 2 .
+ * Вивести в консоль результати операцій &, |, ^ між цими змінними та результат  ~ першої змінної;
+ */
+public class Lesson3_6 {
+    public static void main (String [] args) {
+
+        int firstValue = 4;
+        int secondValue = 2;
+
+    }
+}

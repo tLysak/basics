f6510ae8c49b16f24caa41a36b8b78089679f9f6
sgrey2001 <Xnjpfgbpltw1>
2016-04-25 09:37:26 +0300

    Lesson3

--- src/com/brainacad/skozigon/primitives/Lesson3_7.java
+++ src/com/brainacad/skozigon/primitives/Lesson3_7.java
@@ -5,9 +5,9 @@ import java.util.Random;
 
 public class Lesson3_7 {
     public static void main(String[] args) {
-        int i;
-        System.out.print("i = ");
-        System.out.println((((new Random().nextInt((int) System.currentTimeMillis())) % 2 > 0)) ? (i = 10) : (i = 20));
+        int i = (((new Random().nextInt((int) System.currentTimeMillis())) % 2 > 0)) ? (i = 10) : (i = 20);
+        System.out.println("Integer i = " + i);
+
 
     }
 }

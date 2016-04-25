d5701e06b8c509d77bc489d0c52da2881e281902
alexkaniv <alexkaniv"ukr.net>
2016-04-25 16:51:19 +0300

    Lesson3_2

--- src/com/brainacad/onesterenko/lesson2.java
+++ src/com/brainacad/onesterenko/lesson2.java
@@ -7,14 +7,12 @@ import java.util.Scanner;
  */
 public class lesson2 {
     public static void main(String[] args) {
-       Scanner scan = new Scanner(System.in); //
-         System.out.println("Enter your first line here");
+       Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your first line here");
         String firstln = scan.nextLine();
-            System.out.println("Enter your second line here");
+        System.out.println("Enter your second line here");
         String secondln = scan.nextLine();
-       // String space = " "; // Was it possible to do it without this line (ex. System.out.println (firstln + " " +secondln)- didn't work )
-        //System.out.println ("We are merging the lines for you. Please wait...");
-            System.out.println( firstln + " " + secondln);
+        System.out.println( firstln + " " + secondln);
     }
 
 }
---
+++ src/com/brainacad/onesterenko/primitives/Lesson3_1.java
@@ -0,0 +1,27 @@
+package com.brainacad.onesterenko.primitives;
+
+/**
+ * Created by alexkaniv on 25/04/2016.
+ */
+public class Lesson3_1 {
+    public static void main (String [] args) {
+        byte byteValue = 14;
+        System.out.println("Byte value = " + byteValue);
+        short shortValue = 0472;
+        System.out.println ("Short value = " + shortValue);
+        int intValue = 'Z';
+        System.out.println("Int value = " + intValue);
+        long longValue = 4999898789786542229L;
+        System.out.println("Long value = " +longValue);
+        float floatValue = 0x32E7E^2;
+        System.out.println("Float value = " +floatValue);
+        double doubleValue = 0.785f;
+        System.out.println("Double value = "+ doubleValue);
+        char charValue = 0x5a;
+        System.out.println("Char value = "+ charValue);
+        boolean booleanValue = true;
+        System.out.println ("Boolean value = "+booleanValue);
+
+
+    }
+}

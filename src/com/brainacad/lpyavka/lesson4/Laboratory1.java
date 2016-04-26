b06078645376a84d43cd96ccdc4ed2b9876e0746
Lena <lpyavka@gmail.com>
2016-04-26 20:22:19 +0300

    lesson4

--- src/com/brainacad/lpyavka/lesson4/Laboratory1.java
+++ src/com/brainacad/lpyavka/lesson4/Laboratory1.java
@@ -1,16 +1,5 @@
 package com.brainacad.lpyavka.lesson4;
 
-//Write a program to produce on console the following :
-//
-//        1
-//        2  1
-//        3 2  1
-//        4 3 2  1
-//        5 4 3 2  1
-//        6 5 4 3 2  1
-//        7 6 5 4 3 2  1
-//        8 7 6 5 4 3 2  1
-
 public class Laboratory1 {
     public static void main(String[] args) {
         for (int i = 1; i <= 8; i++) {
--- src/com/brainacad/lpyavka/lesson4/Laboratory2.java
+++ src/com/brainacad/lpyavka/lesson4/Laboratory2.java
@@ -2,9 +2,6 @@ package com.brainacad.lpyavka.lesson4;
 
 import java.util.Scanner;
 
-/**
- * Created by Sony on 25.04.2016.
- */
 public class Laboratory2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
@@ -12,9 +9,9 @@ public class Laboratory2 {
         int value = scan.nextInt();
 
         if (value <= 3) {
-            System.out.println("Value is lower than 3");
+            System.out.println("Value is less than 3");
         } else if (value > 3 && value <= 10) {
-            System.out.println("Value is from 3 to 10");
+            System.out.println("Value is bigger than 3 but less than 10");
         } else {
             System.out.println("Value is bigger than 10");
         }

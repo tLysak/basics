78ca9976bff5e60d07df8ca5ceacfb6f686bae16
sgrey2001 <Xnjpfgbpltw1>
2016-04-27 19:15:06 +0300

    Lesson4

--- src/com/brainacad/skozigon/Lesson4/Lesson4_4.java
+++ src/com/brainacad/skozigon/Lesson4/Lesson4_4.java
@@ -5,7 +5,7 @@ public class Lesson4_4 {
     public static void main(String[] args) {
         int i , j;
             for (j=1 ,i=1; j < 300 & i<=10 ; j++) {
-                if (((j % 3) == 0) && (((j % 4)) == 0)) {
+                if (((j % 3) == 0) && ((j % 4) == 0)) {
                     System.out.println("The number " + i + ", which divide by three and four is: " + j);
                     i++;
                 }

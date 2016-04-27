5ad9c96f9f63abbe5909d0103e54fd2990f26e9d
AGudyma <crowley14@gmail.com>
2016-04-27 21:06:47 +0300

    lesson4

---
+++ src/com/brainacad/agudyma/lesson5/massive.java
@@ -0,0 +1,24 @@
+package com.brainacad.agudyma.lesson5;
+
+import java.util.Arrays;
+
+/**
+ * Created by User on 27.04.2016.
+ */
+public class massive {
+    public static void main(String[]arg){
+        int startV = 1;
+        int endV = 50;
+        int arrLenghth = endV/2;
+        int[] result = new int[arrLenghth];
+        for (int i  =1, currentID = 0 ; i<= endV; i++){
+            if(i%2 == 0) {
+                result[currentID] =i;
+                currentID++;
+
+            }
+
+        }
+        System.out.println(Arrays.toString(result));}
+}
+

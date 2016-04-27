a0466f00aa997b93ec399ef99b07e071caddcca4
sgrey2001 <Xnjpfgbpltw1>
2016-04-27 10:42:46 +0300

    Lesson4

--- src/com/brainacad/skozigon/Lesson4/Lesson4_5.java
+++ src/com/brainacad/skozigon/Lesson4/Lesson4_5.java
@@ -5,11 +5,12 @@ import java.util.Scanner;
 
 public class Lesson4_5 {
     public static void main(String[] args) {
+        System.out.println("Enter your number: ");
         Scanner sc = new Scanner(System.in);
         int i = sc.nextInt();
         int sum = 0, ar_mean = i;
         while (i!=0){
-            sum=sum+i;
+            sum+=i;
             i--;
         }
         System.out.println("Summary of " + ar_mean + " = " + sum);

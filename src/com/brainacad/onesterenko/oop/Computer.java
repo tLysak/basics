a9cad3d5aa34cbf0606e687eb6a33d146e7ccea0
User <alexkaniv@ukr.net>
2016-04-29 22:09:40 +0300

    oop

--- src/com/brainacad/onesterenko/oop/Computer.java
+++ src/com/brainacad/onesterenko/oop/Computer.java
@@ -4,9 +4,58 @@ package com.brainacad.onesterenko.oop;
  * Created by User on 29.04.2016.
  */
 public class Computer {
-    String manufacturer;
-    int serialNumber;
-    float price;
-    int quantityCPU;
-    int frequencyCPU;
+
+
+    private String manufacturer;
+    private int serialNumber;
+    private float price = 120.00f;
+    private int quantityCPU;
+    private int frequencyCPU;
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public int getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(int serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getQuantityCPU() {
+        return quantityCPU;
+    }
+
+    public void setQuantityCPU(int quantityCPU) {
+        this.quantityCPU = quantityCPU;
+    }
+
+    public int getFrequencyCPU() {
+        return frequencyCPU;
+    }
+
+    public void setFrequencyCPU(int frequencyCPU) {
+        this.frequencyCPU = frequencyCPU;
+    }
+
+    public void printStateTOConsole() {
+        System.out.println("COmp serial is " + this.serialNumber + "price is " + this.price);
+    }
 }
+
+
+

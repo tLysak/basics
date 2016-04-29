a16383b573520c007adc8c772b38160e1e2fbca5
User <alexkaniv@ukr.net>
2016-04-29 19:23:48 +0300

    oop

---
+++ src/com/brainacad/onesterenko/oop/Computer.java
@@ -0,0 +1,12 @@
+package com.brainacad.onesterenko.oop;
+
+/**
+ * Created by User on 29.04.2016.
+ */
+public class Computer {
+    String manufacturer;
+    int serialNumber;
+    float price;
+    int quantityCPU;
+    int frequencyCPU;
+}

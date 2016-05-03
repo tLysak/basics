5bf127ac450d3931782777394f64b788d1b6ca36
sgrey2001 <Xnjpfgbpltw1>
2016-05-03 12:36:58 +0300

    Lesson4

---
+++ src/com/brainacad/skozigon/Lesson6/Lesson6_1/MatrixWorker.java
@@ -0,0 +1,16 @@
+package com.brainacad.skozigon.Lesson6.Lesson6_1.;
+
+/**
+ * Created by skozigon on 29.04.2016.
+ */
+public class MatrixWorker {
+    public static void main(String[] args) {
+        Matrix firstMatrix = new Matrix();
+        Matrix secondMatrix = new Matrix();
+        firstMatrix.setState(new int[][] {{1,2},{3,4}});
+        secondMatrix.setState(new int[][] {{1,3}{2,4}});
+
+//        firstMatrix.sum(secondMatrix);
+        System.out.println(firstMatrix.getStringState());
+    }
+}

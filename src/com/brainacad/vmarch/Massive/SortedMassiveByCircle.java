0b5197ae8231995eeaf5a34c8371cf122c4b6e75
vmarch <marchenkoukr@gmail.com>
2016-05-10 03:23:55 +0200

    Adding classes from laboratory works for home analizing

---
+++ src/com/brainacad/vmarch/Massive/SortedMassiveByCircle.java
@@ -0,0 +1,26 @@
+package com.brainacad.vmarch.Massive;
+
+public class SortedMassiveByCircle {
+    public static void main(String[] args) {
+        int[] mass = {3, 8, 6, 9, 12, 23, 2};
+        int a;
+        int temp;
+
+        System.out.println("Unsorted Massive:");
+        for (a = 0; a < mass.length; a++) {
+            System.out.print(mass[a] + " ");
+        }
+        for (a = 1; a < mass.length; a++) {
+            for (int min = 0; min < mass.length - 1; min++)
+                if (mass[min] > mass[min + 1]) {
+                    temp = mass[min + 1];
+                    mass[min + 1] = mass[min];
+                    mass[min] = temp;
+                }
+        }
+        System.out.println("\n\nSorted Massive:");
+        for (a = 0; a < mass.length; a++) {
+            System.out.print(mass[a] + " ");
+        }
+    }
+}
---
+++ src/com/brainacad/vmarch/Massive/SortedMassiveBySort.java
@@ -0,0 +1,13 @@
+package com.brainacad.vmarch.Massive;
+
+import java.util.Arrays;
+
+
+public class SortedMassiveBySort {
+    public static void main(String[] args) {
+
+        int[] mass = {3, 8, 6, 9, 12, 23, 2};
+        Arrays.sort(mass);
+        System.out.print(Arrays.toString(mass));
+    }
+}

3889cecbe05fb9d188ce35e05ca8e6cd8c9cdf69
Lena <lpyavka@gmail.com>
2016-05-15 14:48:51 +0300

    add triangle validation

--- src/com/brainacad/lpyavka/lesson8/Main.java
+++ src/com/brainacad/lpyavka/lesson8/Main.java
@@ -7,7 +7,11 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         Random random = new Random();
-        Triangle triangle = new Triangle(random.nextInt(100), random.nextInt(100), random.nextInt(100));
+        int sideOne = random.nextInt(100);
+        int sideTwo = random.nextInt(100);
+        int sub = Math.abs(sideOne - sideTwo);
+        int sideThree = sub + random.nextInt(sideOne + sideTwo - sub);
+        Triangle triangle = new Triangle(sideOne, sideTwo, sideThree);
         System.out.println(triangle.toString());
         System.out.println("Triangle area is " + triangle.getArea()+ " .");
     }

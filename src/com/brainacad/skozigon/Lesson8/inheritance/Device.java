a07d62e9d77e1c7f9df0ae71f90e968526955cb7
sgrey2001 <Xnjpfgbpltw1>
2016-05-18 18:00:51 +0300

    Lesson 8_HW

--- src/com/brainacad/skozigon/Lesson8/inheritance/Device.java
+++ src/com/brainacad/skozigon/Lesson8/inheritance/Device.java
@@ -1,24 +1,37 @@
 package com.brainacad.skozigon.Lesson8.inheritance;
 
+
+
 /**
  * Created by skozigon on 13.05.2016.
  */
 public class Device {
+    public Device(double price, long serialNumber, String manufacture) {
+        this.price = price;
+        this.serialNumber = serialNumber;
+        this.manufacture = manufacture;
+        System.out.println("in constructor \n" +"Device: " + "manufacture: " + manufacture + "\nserial number: " + serialNumber + "\nprice: " + price);
+    }
+
     private double price = 100;
     private long serialNumber = 2547896332547l;
     private String manufacture = "HP";
+
+    public Device() {
+        System.out.println("in empty constructor");
+
+    }
+
 
     public double getPrice() {
         return price;
     }
 
-    public long getSerialNumber()
-    {
+    public long getSerialNumber() {
         return serialNumber;
     }
 
-    public String getManufacture()
-    {
+    public String getManufacture() {
         return manufacture;
     }
 
@@ -37,14 +50,23 @@ public class Device {
 
     @Override
     public String toString() {
-        return "Device: \n" + "manufacture: " + manufacture + "\nserial number: " + serialNumber + "\nprice: " + price ;
+        return "Device: \n" + "manufacture: " + manufacture + "\nserial number: " + serialNumber + "\nprice: " + price;
     }
+
 
     @Override
     public boolean equals(Object obj) {
-        if (getClass()!= obj.getClass())
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return true;
+        Device device = (Device) obj;
+        if ((price != device.price) && (serialNumber != device.getSerialNumber()) && (manufacture != device.manufacture))
             return false;
-
         return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (Double.doubleToLongBits(price)+(int)(serialNumber^(serialNumber>>>32)) + manufacture.hashCode());
+        return result*31;
     }
 }

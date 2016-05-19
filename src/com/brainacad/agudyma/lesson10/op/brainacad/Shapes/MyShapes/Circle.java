300984d53e12f88336fdbc3d58e0283245cdf0cb
Sidhartha <crowley14@gmail.com>
2016-05-20 00:12:52 +0300

    circle area

---
+++ src/com/brainacad/agudyma/lesson10/op/brainacad/Shapes/MyShapes/Circle.java
@@ -0,0 +1,50 @@
+package com.brainacad.agudyma.lesson10.op.brainacad.Shapes.MyShapes;
+
+import static java.lang.Math.PI;
+
+/**
+ * Created by Sidhartha on 19.05.2016.
+ */
+public class Circle extends Shape{
+
+    private double radius = 0.0;
+    private String color;
+
+    public Circle(double radius, String color) {
+        this.radius = radius;
+        this.color = color;
+    }
+
+
+    @Override
+    public String toString() {
+        System.out.println("Color of Circle " + color +","+ " Radius =" + radius);
+        System.out.println("Shape area = " + calcArea());
+
+
+        return getColor();
+    }
+
+    public double calcArea(){
+        double resultArea = PI *Math.pow(radius,2);
+        return resultArea;
+    }
+
+    public String setColor(String color) {
+        this.color = color;
+        return color;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+
+    }
+}
---
+++ src/com/brainacad/agudyma/lesson10/op/brainacad/Shapes/MyShapes/Main.java
@@ -0,0 +1,12 @@
+package com.brainacad.agudyma.lesson10.op.brainacad.Shapes.MyShapes;
+
+/**
+ * Created by Sidhartha on 19.05.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Circle circle1  = new Circle(11, "red");
+        System.out.println("This is Circle " + circle1);
+              ;
+    }
+}

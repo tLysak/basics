3864f5a3b39b4a86b4e6672c3665791d59b63b3f
AGudyma <crowley14@gmail.com>
2016-05-20 19:50:01 +0300

    lesson11

---
+++ src/com/brainacad/agudyma/lesson11/AbstractClasses/Circle.java
@@ -0,0 +1,17 @@
+package com.brainacad.agudyma.lesson11.AbstractClasses;
+
+/**
+ * Created by User on 20.05.2016.
+ */
+public class Circle extends Shape {
+
+
+    double radius = 0.0;
+
+public Circle (double radius, String colour){
+    this.radius = radius;
+    this.colour = colour;
+}
+}
+
+

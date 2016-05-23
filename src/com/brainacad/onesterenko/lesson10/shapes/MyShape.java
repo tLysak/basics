3a534e52deda3b0d0b1d971b01079ddbe64858d8
alexkaniv <alexkaniv@ukr.net>
2016-05-23 14:04:17 +0300

    lsn10

---
+++ src/com/brainacad/onesterenko/lesson10/shapes/MyShape.java
@@ -0,0 +1,12 @@
+package com.brainacad.onesterenko.lesson10.shapes;
+
+/**
+ * Created by user on 23.05.2016.
+ */
+public class MyShape {
+    String color;
+    double area;
+    public double calcArea  (){
+        return area;
+    }
+}

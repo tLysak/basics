704916c6f423ab5d7ae0eebf6841b617c8f90f08
Sidhartha <crowley14@gmail.com>
2016-05-31 23:38:02 +0300

    raw multiplication

--- src/com/brainacad/agudyma/oop/testRegExp/Main.java
+++ src/com/brainacad/agudyma/oop/testRegExp/Main.java
@@ -1,5 +1,6 @@
 package com.brainacad.agudyma.oop.testRegExp;
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -13,37 +14,40 @@ public class Main {
 //        this.personNames = personNames;
 //        return personNames;
 //    }
-//
 ////
+//////
 //    public static boolean checkPersonWithRegExp() {
 //        this.arr1 =arr1;
 //        for (String element : arr1) {
 //            System.out.println(element);
-//
-//
+////
+////
 //
 //        }
 //        return true;
-//    }
+////    }
 
 
     public static void main(String[] args) {
 
-        String [] arr1 = {"VOVA","Ivan","R2d2","ZX","Anna","12345","ToAd","TomCat", " "};
-        String personMName = arr1[1];
-        System.out.println(personMName);
-        String pattern = "(.*)(\\d+)(.*)";
-        Pattern pattern1 = Pattern.compile(pattern);
-        Matcher m = pattern1.matcher(personMName);
-        System.out.println(pattern1);
-        System.out.println(m.matches());
-        for (String element:arr1 ){
-//            int i;
-//            if( arr1[i] )
-//            System.out.println(element);
+        String[] arr1 = {"VOVA", "Ivan", "R2d2", "ZX", "Anna", "12345", "ToAd", "TomCat", " "};
+
+        System.out.println(Arrays.toString(arr1));
+
+//        for (String element : arr1) {
+            for (int i = 0; i < arr1.length; i++) {
+                String personName = arr1[i];
+//                System.out.println(personName);
+                String pattern = "(^[A-Z][a-z]+$)";
+                Pattern pattern1 = Pattern.compile(pattern);
+                Matcher m = pattern1.matcher(personName);
+                if (m.matches()) {
+                    System.out.println(personName);
+                }
+//                System.out.println(m.matches());
+
+
         }
-//        System.out.println(checkPersonWithRegExp());
     }
-
-
 }
+

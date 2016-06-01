8770caa60038ccd0b5291c6df6256ccbefb2903a
User <alexkaniv@ukr.net>
2016-06-01 19:12:25 +0300

    lsn14+13=TODO

--- src/com/brainacad/lpyavka/lesson11/Shapes/MainShape.java
+++ src/com/brainacad/lpyavka/lesson11/Shapes/MainShape.java
@@ -1,14 +1,2 @@
 package com.brainacad.lpyavka.lesson11.Shapes;
 
-import com.brainacad.onesterenko.lesson11.Triangle;
-
-public class MainShape {
-    public static void main(String[] args) {
-        Circle circle = new Circle("red", 13);
-        Triangle triangle = new Triangle();
-        circle.calcArea();
-        triangle.calcArea();
-        System.out.println(circle.toString());
-        System.out.println(triangle.toString());
-    }
-}

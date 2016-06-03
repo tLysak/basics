d6af260f9fcc288ef3bfd4696cda11f9ddeeffda
Aleksandr Kuziakov <akuziakov@gmail.com>
2016-06-03 21:04:04 +0300

    MyPhoneBook - sortByName() and sortByPhone() updated with lambda expression

--- src/com/brainacad/akuziakov/lesson15/testnested1/Main.java
+++ src/com/brainacad/akuziakov/lesson15/testnested1/Main.java
@@ -2,24 +2,28 @@ package com.brainacad.akuziakov.lesson15.testnested1;
 
 public class Main {
     public static void main(String[] args) {
-        MyPhoneBook myPhoneBook = new MyPhoneBook();
+        MyPhone myPhone=new MyPhone();
+        myPhone.switchOn();
+        myPhone.call(1);
 
-        for (int i = 5; i >= 1; i--) {
-            try {
-                myPhoneBook.addPhoneNumber("Number" + i, "+38093 " + i + i + i + "-" + i + i + "-" + i + i);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-        myPhoneBook.printPhoneBook();
-
-        System.out.println("\n\n\n");
-
-        myPhoneBook.sortByName();
-        myPhoneBook.printPhoneBook();
-        System.out.println("\n\n\n");
-        myPhoneBook.sortByPhoneNumber();
-        myPhoneBook.printPhoneBook();
+//        MyPhone.MyPhoneBook myPhoneBook = new MyPhoneBook();
+//
+//        for (int i = 5; i >= 1; i--) {
+//            try {
+//                myPhoneBook.addPhoneNumber("Number" + i, "+38093 " + i + i + i + "-" + i + i + "-" + i + i);
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        }
+//
+//        myPhoneBook.printPhoneBook();
+//
+//        System.out.println("\n\n\n");
+//
+//        myPhoneBook.sortByName();
+//        myPhoneBook.printPhoneBook();
+//        System.out.println("\n\n\n");
+//        myPhoneBook.sortByPhoneNumber();
+//        myPhoneBook.printPhoneBook();
     }
 }

19cf56d359812103ff76d51776b0330b3cac08a3
vmarch <marchenkoukr@gmail.com>
2016-06-13 01:55:01 +0200

    Adding classes from laboratory works for home analizing

---
+++ src/com/brainacad/vmarch/lesson10/testshapes/MainShapes.java
@@ -0,0 +1,16 @@
+package com.brainacad.vmarch.lesson10.testshapes;
+
+
+public class MainShapes {
+    public static void main(String[] args) {
+
+
+        Circle circle = new Circle("RED", 20.0);
+        System.out.println(circle);
+
+        Rectangle rectangle = new Rectangle(10.0, 12.0);
+        System.out.println(rectangle);
+
+
+    }
+}
---
+++ src/com/brainacad/vmarch/lesson10/testshapes/Shape.java
@@ -0,0 +1,20 @@
+package com.brainacad.vmarch.lesson10.testshapes;
+
+public class Shape {
+    String shapeColor;
+    String shapeName;
+
+    Shape() {
+    }
+
+    public double calcArea() {
+        return 0.0;
+    }
+
+    public String toString() {
+        return "This is " + shapeName + ", color is: " + shapeColor;
+
+    }
+
+
+}

1749a4ddcc570bbb6497f8d6faef67dad110abeb
vmarch <marchenkoukr@gmail.com>
2016-06-13 22:00:24 +0200

    Adding classes from laboratory works for home analizing

--- src/com/brainacad/vmarch/lesson19/testcollection1/MainTestCollection1.java
+++ src/com/brainacad/vmarch/lesson19/testcollection1/MainTestCollection1.java
@@ -1,17 +1,5 @@
 package com.brainacad.vmarch.lesson19.testcollection1;
 
-//        Add to method main() code that declares “list” variable (of List type) of string objects as ArrayList<String>.
-//        Write code to fill this “list” in loop with string values like: “number_”
-//        and number of current iteration (from 0 to 10).
-//        Add code which iterates over list and print current element value to console.
-//        Execute the program.
-//        The program output must be like next example:
-//        number_0
-//        number_1
-//        number_2
-//        …
-//        number_10
-
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;

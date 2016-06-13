b0300ad66e0833a86bc23133a6e9363389e602ae
sgrey2001 <Xnjpfgbpltw1>
2016-06-13 17:40:16 +0300

    Labs2_14

--- src/com/brainacad/skozigon/Labs/labs2_14/testgenerics1/testgenerics2/Main.java
+++ src/com/brainacad/skozigon/Labs/labs2_14/testgenerics1/testgenerics2/Main.java
@@ -5,12 +5,14 @@ package com.brainacad.skozigon.Labs.labs2_14.testgenerics1.testgenerics2;
  */
 public class Main {
     public static void main(String[] args) {
-        Integer [] int_mass = {1, 2, 10, 58, 43, 27, 98, 100};
-        Double[] double_mass = {1.2, 5.4, 4.8};
+        Integer[] int_mass = {1, 2, 10, 58, 43, 27, 98, 100};
+        Double[] double_mass = {1.2, 5.4, 4.8, 0.8, 1.3, 0.3, 1.1, 2.6};
 
-        System.out.println(MyTestMethod.calckNum(int_mass, 10));
+        System.out.println("Number of elements that are greater than a 10: " + MyTestMethod.calckNum(int_mass, 10));
+        System.out.println("Number of elements that are greater than a 1.2: " + MyTestMethod.calckNum(double_mass,1.2d));
 
-
+        System.out.println("Summary of elements that are greater than a 10: " + MyTestMethod.calckSum(int_mass, 10));
+        System.out.println("Summary of elements that are greater than a 1.2: " + MyTestMethod.calckSum(double_mass, 1.2));
 
     }
 }
--- src/com/brainacad/skozigon/Labs/labs2_14/testgenerics1/testgenerics2/MyTestMethod.java
+++ src/com/brainacad/skozigon/Labs/labs2_14/testgenerics1/testgenerics2/MyTestMethod.java
@@ -1,11 +1,11 @@
 package com.brainacad.skozigon.Labs.labs2_14.testgenerics1.testgenerics2;
 
-import java.util.Comparator;
-
 /**
  * Created by skozigon on 06.06.2016.
  */
 public class MyTestMethod  {
+
+
 
     public static <T extends Comparable> int calckNum(T[] mass, T el){
         int i=0;
@@ -17,5 +17,24 @@ public class MyTestMethod  {
         return i;
     }
 
+    public static <T extends Number & Comparable > Double calckSum(Double[] mass, T el){
+        Double sum = Double.valueOf(0);
+        for (Double t: mass) {
+            if (t.compareTo((Double) el)> 0) {
+                sum = sum+t;
+            }
+        }
+        return sum;
+    }
+
+    public static <T extends Number & Comparable > Integer calckSum(Integer[] mass, T el){
+        Integer sum = Integer.valueOf(0);
+        for (Integer t: mass) {
+            if (t.compareTo((Integer) el)> 0) {
+                sum = sum+t;
+            }
+        }
+        return sum;
+    }
 
 }

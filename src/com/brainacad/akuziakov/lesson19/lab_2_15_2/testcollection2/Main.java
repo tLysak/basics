7643a9702ab1b815d0298994a5a3fb1d59cd3781
Aleksandr Kuziakov <akuziakov@gmail.com>
2016-06-13 22:01:31 +0300

    Lab Work 2-15-3 - not complete

---
+++ src/com/brainacad/akuziakov/lesson19/lab_2_15_2/testcollection2/Main.java
@@ -0,0 +1,28 @@
+package com.brainacad.akuziakov.lesson19.lab_2_15_2.testcollection2;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<String> list=new LinkedList<String>();
+
+        Random random=new Random();
+
+        int index;
+        for (int i=0;i<=10;i++){
+            index=Math.abs(random.nextInt(10));
+            list.add(index, "number_"+i);
+        }
+
+        Iterator<String> stringIterator=list.iterator();
+
+        while (stringIterator.hasNext()){
+            System.out.println(stringIterator.next());
+        }
+    }
+}

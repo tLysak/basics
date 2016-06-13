a9560f0586456104e48ad26e48a6cfcd382b40c0
AGudyma <crowley14@gmail.com>
2016-06-13 20:42:14 +0300

    myPhone

--- src/com/brainacad/agudyma/oop/testNested1/Main.java
+++ src/com/brainacad/agudyma/oop/testNested1/Main.java
@@ -10,23 +10,25 @@ import static com.brainacad.agudyma.oop.testNested1.MyPhone.*;
  */
 public class Main {
     public static void main(String[] args) {
-
-        MyPhone.MyPhoneBook phoneBook = new MyPhone.MyPhoneBook();
-        phoneBook.addPhoneNumber("Sasha","23537431451");
-        phoneBook.addPhoneNumber("Dima","145136222");
-        phoneBook.addPhoneNumber("Borat","2351345147" );
-        phoneBook.addPhoneNumber("Givi","845134626");
-        phoneBook.addPhoneNumber("Mimino","134513465");
-        phoneBook.addPhoneNumber("vmarch","13461466");
-        phoneBook.addPhoneNumber("Rabinovich","41461461");
-        phoneBook.addPhoneNumber("Malevich","313461616" );
-        phoneBook.addPhoneNumber("Abramovich","913461361");
-        phoneBook.addPhoneNumber("Zuckerman","01461616");
+        MyPhone myPhone = new MyPhone();
+        myPhone.swichOn();
+//
+//        MyPhone.MyPhoneBook phoneBook = new MyPhone.MyPhoneBook();
+//        phoneBook.addPhoneNumber("Sasha","23537431451");
+//        phoneBook.addPhoneNumber("Dima","145136222");
+//        phoneBook.addPhoneNumber("Borat","2351345147" );
+//        phoneBook.addPhoneNumber("Givi","845134626");
+//        phoneBook.addPhoneNumber("Mimino","134513465");
+//        phoneBook.addPhoneNumber("vmarch","13461466");
+//        phoneBook.addPhoneNumber("Rabinovich","41461461");
+//        phoneBook.addPhoneNumber("Malevich","313461616" );
+//        phoneBook.addPhoneNumber("Abramovich","913461361");
+//        phoneBook.addPhoneNumber("Zuckerman","01461616");
 
 //        phoneBook.printPhoneBook();
 
 //        phoneBook.addPhoneNumber("ssss","8646");
-        MyPhone.swichOn();
+
         MyPhone.call();
     }
 }

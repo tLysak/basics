d45b70591ac64707653df30b47b2d6d20b1b41cd
Aleksandr Kuziakov <akuziakov@gmail.com>
2016-06-17 21:33:00 +0300

    overload resolution labs - complete

---
+++ src/com/brainacad/akuziakov/lesson19/quetest1/main.java
@@ -0,0 +1,10 @@
+package com.brainacad.akuziakov.lesson19.quetest1;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) {
+        Queue<Integer> integerQueue=new LinkedList<>();
+    }
+}
---
+++ src/com/brainacad/akuziakov/lesson21/lab_2_18_4/Animal.java
@@ -0,0 +1,10 @@
+package com.brainacad.akuziakov.lesson21.lab_2_18_4;
+
+class Animal {
+}
+
+class Dog extends Animal{
+}
+
+class Puppy extends Dog{
+}

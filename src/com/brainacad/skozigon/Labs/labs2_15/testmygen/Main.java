56b1c2a81caffde451ef03dfc813fe653e6c6830
sgrey2001 <Xnjpfgbpltw1>
2016-06-17 18:03:43 +0300

    Labs2_15_6

--- src/com/brainacad/skozigon/Labs/labs2_15/testmygen/Main.java
+++ src/com/brainacad/skozigon/Labs/labs2_15/testmygen/Main.java
@@ -6,6 +6,11 @@ package com.brainacad.skozigon.Labs.labs2_15.testmygen;
 public class Main {
     public static void main(String[] args) {
         MyNumGenerator myNumGenerator = new MyNumGenerator(10, 100);
+
+        System.out.println("Execute generate() method: ");
         System.out.println(myNumGenerator.generate());
+
+        System.out.println("Execute generateDistinct() method:");
+        System.out.println(myNumGenerator.generateDistinct());
     }
 }

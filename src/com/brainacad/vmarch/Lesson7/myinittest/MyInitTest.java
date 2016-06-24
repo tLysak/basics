90a7c4cc5efbd48dac0e7d178a5acfa98d26107d
vmarch <marchenkoukr@gmail.com>
2016-06-24 19:08:56 +0200

    Adding classes from laboratory works for home analizing

--- src/com/brainacad/vmarch/Lesson7/myinittest/MyInitTest.java
+++ src/com/brainacad/vmarch/Lesson7/myinittest/MyInitTest.java
@@ -1,20 +1,5 @@
 package com.brainacad.vmarch.Lesson7.myinittest;
 
-//Create class “MyInitTest” with two initialization blocks,
-// two static initialization blocks and two constructors
-// (one constructor() must invoke second constructor(int)).
-//        On each block write code that prints on console type of block and block number.
-//
-////        For example:
-//        “static initialization block 1”
-//        “static initialization block 2”
-//        “non-static initialization block 1”
-//        “non-static initialization block 2”
-//        “constructor 1”
-//        “constructor 2”
-
-//Create class Main, which will create instance of class MyInitTest by invoking first constructor.
-
 
 public class MyInitTest {
 

88bbddd23645f9d124059b06d6648ab79671c257
Sidhartha <crowley14@gmail.com>
2016-06-26 19:57:51 +0300

    MySumCount

--- src/com/brainacad/agudyma/oop/testThread3/MySumCount.java
+++ src/com/brainacad/agudyma/oop/testThread3/MySumCount.java
@@ -1,13 +1,32 @@
 package com.brainacad.agudyma.oop.testThread3;
 
+import java.util.Arrays;
+import java.util.Random;
+
 /**
  * Created by Sidhartha on 24.06.2016.
  */
-public class MySumCount extends Thread{
-    public  int startIndex;
+public class MySumCount extends Thread {
+
+    private static int sum;
+    public int startIndex;
     public int stopIndex;
+    public  int [] arrOfInt;
     public long resultSum;
-    public int [] arrOfInt = new int[1000];
+
+    public long getResultSum() {
+        return resultSum;
+    }
+
+
+    public int[] getArrOfInt() {
+        return arrOfInt;
+    }
+
+    public void setArrOfInt(int[] arrOfInt) {
+        this.arrOfInt = arrOfInt;
+    }
+
 
     public int getStartIndex() {
         return startIndex;
@@ -22,35 +41,71 @@ public class MySumCount extends Thread{
     }
 
     public void setStopIndex(int stopIndex) {
+
         this.stopIndex = stopIndex;
     }
 
     @Override
-    public void run(){
-        for (int i = 0; i > 10;i++){
+    public void run() {
+        int f  = getStartIndex();
 
-            System.out.println(i);
-        }
-        try{
-            sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        for ( getStartIndex() ; getStartIndex()< stopIndex;f++)
+            resultSum += arrOfInt[f];
 
-    }
-
-    public int sumOfArrayElements(){
-
-        return 0;
-    }
-
-    public static void main(String[] args) {
+//        while (getStartIndex() < getStopIndex()) {
+//            {
+//                resultSum += getArrOfInt()[startIndex++];
+//
+//            }
+//    System.out.println("sum " + getResultSum());
 
     }
 
 
+//    public long sumOfArrayElements() {
+//
+//                while (getStartIndex() < getStopIndex()) {
+//            {
+//                resultSum += getArrOfInt()[startIndex++];
+//            }
+//        }
+//        return resultSum;
+//    }
+
+    public static void main(String[] args) throws InterruptedException {
+
+        int[] arrOfInt = new int[100];
+        Random random = new Random(99);
+        for (int i = 0; i < arrOfInt.length; i++) {
+            arrOfInt[i] = random.nextInt(99);
+                }
+        MySumCount mySumCount = new MySumCount();
+
+        mySumCount.setStartIndex(0);
+        System.out.println(mySumCount.getStartIndex());
+
+        mySumCount.setStopIndex(5);
+        System.out.println(mySumCount.getStopIndex());
+
+        mySumCount.setArrOfInt(arrOfInt);
+        System.out.println(Arrays.toString(mySumCount.getArrOfInt()));
+
+//        System.out.println(mySumCount.sumOfArrayElements());
+//
+//        mySumCount.sumOfArrayElements();
 
 
+        Thread thr1 = new MySumCount();
+
+//        Thread thr2 = new MySumCount();
+
+        thr1.start();
+        System.out.println(mySumCount.getResultSum());
+        thr1.join();
 
 
+    }
 }
+
+
+

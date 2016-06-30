feca48cf60b8de7d6434af3ba0cd5c4f2943efca
vmarch <marchenkoukr@gmail.com>
2016-06-30 02:35:33 +0200

    Adding classes from laboratory works for home analizing

--- src/com/brainacad/vmarch/lesson16/testnested1/MyPhoneBook.java
+++ src/com/brainacad/vmarch/lesson16/testnested1/MyPhoneBook.java
@@ -1,16 +1,15 @@
 package com.brainacad.vmarch.lesson16.testnested1;
 
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class MyPhoneBook {
-
-    int a = 10;
-    int i = 0;
+    private int a = 10;
+    private int i = 0;
 
     private PhoneNumber[] phoneNumbers = new PhoneNumber[a];
 
-
     public void addPhoneNumber(String name, String phone) {
-
 
         PhoneNumber phoneNumber = new PhoneNumber(name, phone);
 
@@ -18,54 +17,73 @@ public class MyPhoneBook {
             phoneNumbers[i] = phoneNumber;
             System.out.println(phoneNumbers[i]);
 
-
             if (i < (a - 1)) i++;
 
         } else if (i < a - 1) {
             i++;
-        } else {
-            System.out.println("memory is full");
 
+        } else {
+
+            System.out.println("memory is full");
         }
     }
 
-//    public void printPhoneBook() {
-//        System.out.println(Arrays.toString(phoneNumbers));
-//    }
+    public void sortByName() {
+        Arrays.sort(phoneNumbers, new Comparator<PhoneNumber>() {
+            @Override
+            public int compare(PhoneNumber o1, PhoneNumber o2) {
+                return o1.getName().compareTo(o2.getName());
+
+            }
+        });
+    }
+
+    public void sortByPhoneNumber() {
+        Arrays.sort(phoneNumbers, new Comparator<PhoneNumber>() {
+            @Override
+            public int compare(PhoneNumber o1, PhoneNumber o2) {
+                return o1.getPhone().compareTo(o2.getPhone());
+
+            }
+        });
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(phoneNumbers);
+    }
 
 
-    public static class PhoneNumber {
+    private static class PhoneNumber {
 
+        private String name;
+        private String phone;
 
-        String name;
-        String phone;
-
-        public PhoneNumber(String name, String phone) {
+        private PhoneNumber(String name, String phone) {
             this.name = name;
             this.phone = phone;
         }
 
-        public String getName() {
+        private String getName() {
             return name;
         }
 
-        public void setName(String name) {
+        private void setName(String name) {
             this.name = name;
         }
 
-        public String getPhone() {
+        private String getPhone() {
             return phone;
         }
 
-        public void setPhone(String phone) {
+        private void setPhone(String phone) {
             this.phone = phone;
         }
 
         @Override
         public String toString() {
-            return "name: " + name + ", phone: " + phone;
+            return "Name: " + name + ", Phone: " + phone;
 
         }
     }
 }
-

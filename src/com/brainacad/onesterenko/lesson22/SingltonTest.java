583f4bb40a74a1d3716758c833a0aea4ed2f5d9d
User <alexkaniv@ukr.net>
2016-07-04 22:00:23 +0300

    lsn tests

---
+++ src/com/brainacad/onesterenko/lesson22/SingltonTest.java
@@ -0,0 +1,13 @@
+package com.brainacad.onesterenko.lesson22;
+
+/**
+ * Created by User on 04/07/2016.
+ */
+public class SingltonTest {
+    public static void main(String[] args) {
+        SingleObject obj = new SingleObject();
+        SingleObject object = SingleObject.getInstance();
+        object.showMessage();
+
+    }
+}

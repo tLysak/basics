769c32a0ccfd64c49c9967549bd8d45d29dbca25
AGudyma <crowley14@gmail.com>
2016-07-04 22:02:07 +0300

    NumberFormat

---
+++ src/com/brainacad/agudyma/singleTon/Main.java
@@ -0,0 +1,12 @@
+package com.brainacad.agudyma.singleTon;
+
+/**
+ * Created by User on 04/07/2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        SingleObject object = SingleObject.getInstance();
+
+    }
+}

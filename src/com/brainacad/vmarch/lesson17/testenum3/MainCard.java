414b546ef3f4dbb9ce28117d60dd56cb81758901
vmarch <marchenkoukr@gmail.com>
2016-07-07 23:02:31 +0200

    Adding classes from laboratory works for home analizing

---
+++ src/com/brainacad/vmarch/lesson17/testenum3/MainCard.java
@@ -0,0 +1,13 @@
+package com.brainacad.vmarch.lesson17.testenum3;
+
+
+public class MainCard {
+
+    public static void main(String[] args) {
+
+        Card card = new Card("ranc", "suit");
+
+        System.out.println(card);
+    }
+}
+
---
+++ src/com/brainacad/vmarch/lesson17/testenum3/SuitEnum.java
@@ -0,0 +1,9 @@
+package com.brainacad.vmarch.lesson17.testenum3;
+
+
+public enum SuitEnum {
+
+    SPADE, DIAMOND, CLUB, HEART
+
+}
+

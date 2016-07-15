cf69568ea1bee04684cc519673070a7b28252822
marchenkoukr@gmail.com <123123123ukr>
2016-07-16 01:53:06 +0300

    Just for learning of Java.

--- src/com/brainacad/vmarch/lesson18/testgenerics2_3/MainMyTestMethod.java
+++ src/com/brainacad/vmarch/lesson18/testgenerics2_3/MainMyTestMethod.java
@@ -1,4 +1,4 @@
-package com.brainacad.vmarch.lesson18.testgenerics2;
+package com.brainacad.vmarch.lesson18.testgenerics2_3;
 
 
 public class MainMyTestMethod {
@@ -9,7 +9,10 @@ public class MainMyTestMethod {
         Double[] arrDoub = {6.5, 4.7, 5.2, 6.4, 7.8, 8.9, 9.9, 3.5};
 
         MyTestMethod.calcNum(arrInteg, 5);
+        MyTestMethod.calcSum(arrInteg, 5);
+
         MyTestMethod.calcNum(arrDoub, 6.3);
+        MyTestMethod.calcSum(arrDoub, 6.3);
 
     }
 }

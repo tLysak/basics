f8228716306d9407f50f855157f788bbfe466401
tymur <ntvf@ukr.net>
2016-07-15 16:59:22 +0300

    First test - adding matrix

--- src/test/java/com/brainacad/tlysak/oop/MatrixTest.java
+++ src/test/java/com/brainacad/tlysak/oop/MatrixTest.java
@@ -4,22 +4,26 @@ package com.brainacad.tlysak.oop;
 import com.brainacad.tlysak.oop.Matrix;
 import org.junit.Test;
 
-import java.util.Arrays;
+import static org.junit.Assert.assertArrayEquals;
 
 /**
  * Created by tymur on 7/1/16.
  */
 public class MatrixTest {
+
     @Test
     public void summTest() {
         Matrix matrix1 = new Matrix();
-        matrix1.setState(new int[][]{{1,1},{1,1}});
+        matrix1.setState(new int[][]{{1,1},
+                                     {1,1}});
 
         Matrix matrix2 = new Matrix();
-        matrix1.setState(new int[][]{{2,2},{2,2}});
+        matrix2.setState(new int[][]{{2,12},
+                                     {-5,2}});
 
         matrix1.summ(matrix2);
-        Arrays.toString(matrix1.getState());
+        assertArrayEquals(matrix1.getState(), new int[][]{{3,13},
+                                                                 {-4,3}});
 
 
     }
